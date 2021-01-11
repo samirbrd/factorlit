7d4a15956a198d7fa4889280baa2277046594e47
samirbrd <samirbrd@gmail.com>
2021-01-11 12:50:18 +0000

    msg

--- factorsstreamlit.py
+++ factorsstreamlit.py
@@ -16,7 +16,7 @@ st.write('For Investors who want to invest in markets with factor tilts')
 path_string_1="./Factor Backtests/"
 path_string_2="./Factor Backtests/Low Vol/"
 path_string_3="./Factor Selections/"
-df_date=path_string_3+'updated_till.csv'
+df_date=pd.read_csv(path_string_3+'updated_till.csv')
 updated_till=df_date['Date'].loc[0]
 st.write('Updated till'+' '+updated_till)
 dir_1={'High Alpha':'alpha','Low Vol':'vol','Low Beta':'beta','Low iVol':'ivol','High Momentum':'momentum'}
